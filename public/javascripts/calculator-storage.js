6d0a069b921b5095ebd58539d03654befaaaa60c
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-04-13 16:26:16 +0100

    Clear memory bank feature

--- public/javascripts/calculator-storage.js
+++ public/javascripts/calculator-storage.js
@@ -3,6 +3,17 @@ function CalculatorStorage(target, dragValue) {
   this.target = $(target);
   this.dragValue = $(dragValue);
 
+  this.initialize = function() {
+    // bind events to this object
+    var storage = this;
+    $.each(['clearMemoryBank'], function(index, event) {
+      storage[event] = $.bind(storage, storage[event]);
+    });
+
+    $('#memory-bank a#clear-memory-bank').click(this.clearMemoryBank);
+    this.retrieveValues();
+  }
+
   this.setupDropTarget = function() {
     var storage = this;
     this.dragValue.draggable()
@@ -36,15 +47,28 @@ function CalculatorStorage(target, dragValue) {
     var currentlyStored = ($.cookie('stored_values') ? $.cookie('stored_values').split('|') : []);
     currentlyStored.splice(0, 0, value)
     $.cookie('stored_values', currentlyStored.slice(0,this.MAX_ITEMS).join('|'), { expires: 30 });
+    $('#memory-bank a#clear-memory-bank').show();
   }
 
   this.retrieveValues = function() {
     var storage = this;
     var currentlyStored = ($.cookie('stored_values') ? $.cookie('stored_values').split('|') : []);
+    storage.target.find('ul li').remove();
     $.each(currentlyStored, function(index, elem) {
       storage.target.find('ul').append('<li>' + elem + '</li>');
     });
+    if (currentlyStored.length > 0) {
+      $('#memory-bank a#clear-memory-bank').show();
+    } else {
+      $('#memory-bank a#clear-memory-bank').hide();
+    }
   }
 
-  this.retrieveValues();
+  this.clearMemoryBank = function(object, event) {
+    event.preventDefault();
+    $.cookie('stored_values', '');
+    this.retrieveValues();
+  }
+
+  this.initialize();
 }
